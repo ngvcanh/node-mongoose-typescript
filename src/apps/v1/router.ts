b53bdbbaae5ff261f1b53000c812cc4540d559bf
Canh Nguyen Van <ngvcanh2014@gmail.com>
2023-02-27 02:38:36 +0700

    UPDATE :: Using gitmodule add package @serv-ts/express

--- src/apps/v1/router.ts
+++ src/apps/v1/router.ts
@@ -2,10 +2,11 @@ import express from "express";
 
 import users from './users';
 
-export default function getRouter() {
+export default function getRouter(app: express.Express) {
   const router = express.Router();
 
   router.use('/users', users);
 
+  app.use('/api/v1', router);
   return router;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import './core/Env';
+import { Server } from '@serv-ts/express';
 import Database from './core/Database';
-import Server from './core/Server';
+import routes from './apps/v1/router';
+// import Server from './core/Server';
 
 // Create database instance
 const db = Database.getInstance();
 
-// Create server instance
-const server = new Server(db);
 
-// Start the server
+// // Create server instance
+const server = new Server({ db, routes });
+
+// // Start the server
 server.start();
